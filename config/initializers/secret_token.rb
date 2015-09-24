0a9a40493c6aac1ec23d8c8873200e340489f593
teapotzhang <jing1992210@163.com>
2015-09-24 16:06:40 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peiwen::Application.config.secret_token = 'd0a81d9b97710086108c153f922ecce112bd49c544376d1c7629bed96795384ca2b1a79527d56bed0247314c5db4fbf541be8d3a5951eaf1d56671a53498698e'
